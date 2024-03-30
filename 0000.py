d7f43ee057118409875c5ee9f65f64e7738bc754
lennchi <elena.haramulova@gmail.com>
2024-03-30 09:48:13 +0100

    strip(), split(), sets, get()

--- 0000.py
+++ 0000.py
@@ -1,3 +1,10 @@
+## Conversion from binary
+## Math and Statistics imports
+## Cleaning up strings with split() and strip()
+## Sets
+## get()
+
+
 # Conversion from binary
 text = "1001"
 number = int(text, 2)  # 2 for binary
@@ -15,8 +22,8 @@ statistics.mode()
 
 # Cleaning up strings
 string = "jh a sasdf7"
-string.strip()
-string.split()
+string.strip()  # removes leading and trailing whitespaces; can specify chars to remove in ()
+string.split()  # splits string into list items; optional: (sep=delimiter, maxsplit=max_num_of_splits_to_perform)
 
 
 # SETS are unordered; can only have unique values
@@ -25,5 +32,15 @@ hobbies = set(hobbies)
 
 hobbies.add("python")  # adding a new item to set
 
-all_hobbies = hobbies_1 | hobbies_2  # outer join
-common_hobbies = hobbies_1 & hobbies_2  # inner join
+all_hobbies = hobbies_1 | hobbies_2  # outer join (all of both)
+common_hobbies = hobbies_1 & hobbies_2  # inner join (overlap)
+
+
+# GET METHOD =  defense against KeyError
+# this won't work if no such key in dict
+insurance_number = person("insurance_card")
+
+# this will work:
+insurance_number = person.get(
+    "insurance_card", "Naaaah"
+)  # the second arg is what is returned if key not found
