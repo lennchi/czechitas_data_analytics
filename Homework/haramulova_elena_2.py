43b24d28878d3e0751b4c65a5390dc921baf1fe0
lennchi <elena.haramulova@gmail.com>
2024-04-03 12:10:57 +0200

    Handling nans

--- Homework/haramulova_elena_2.py
+++ Homework/haramulova_elena_2.py
@@ -1,10 +1,14 @@
 import json
 import pandas as pd
-from pprint import pprint
 
 
-# Read data from the source file into a pd df
-df = pd.read_csv("./data/netflix_titles.tsv", delimiter="\t")
+# Read data from the source file into a pd df, handle nans
+df = pd.read_csv(
+    "./data/netflix_titles.tsv",
+    encoding="utf-8",
+    delimiter="\t",
+    keep_default_na=False,
+)
 
 # # Fetch column headers as a list so I don't have to copy/type them out one by one later :D
 # headers = df.columns.tolist()
@@ -49,7 +53,24 @@ df = df.rename(columns=new_headers)
 # Reorder columns as per assignment
 df = df[["title", "directors", "cast", "genres", "decade"]]
 
-# Convert df rows into dicts
-movies = [row.to_dict() for header, row in df.iterrows()]
+# Convert strings for directors/cast into lists
+df["directors"] = df["directors"].str.split(",")
+df["cast"] = df["cast"].str.split(",")
 
-pprint(movies)
+# Convert df rows into dicts
+movies = [row.to_dict() for i, row in df.iterrows()]
+
+# Change years to decades
+for movie in movies:
+    year_str = str(movie["decade"])
+    movie["decade"] = year_str[:-1] + "0"
+
+# Replace [""] with [] for directors/cast
+movies = [
+    {key: [] if value == [""] else value for key, value in movie.items()}
+    for movie in movies
+]
+
+# Write to a new .json file
+with open("homework/movies.json", "w", encoding="utf-8") as f:
+    json.dump(movies, f, indent=4)
